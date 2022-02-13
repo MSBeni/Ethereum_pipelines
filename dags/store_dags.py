1decc99fc3dca3819d958cc06b0996137071f8df
unknown <salimibeni@floppy.encs.concordia.ca>
2022-02-12 21:47:39 -0500

    added basic airflow pipelines 3

--- dags/store_dags.py
+++ dags/store_dags.py
@@ -33,6 +33,7 @@ with DAG('store_dag', default_args=default_args, schedule_interval='@daily',
 
     t5 = MySqlOperator(task_id='select_from_table', mysql_conn_id="mysql_conn", sql="select_from_table.sql")
 
+
     t6 = BashOperator(task_id='move_file1', bash_command='cat ~/store_files_airflow/location_wise_profit_2.csv '
                                                          '&& mv ~/store_files_airflow/location_wise_profit_2.csv'
                                                          ' ~/store_files_airflow/location_wise_profit_2_%s.csv'
@@ -55,5 +56,15 @@ with DAG('store_dag', default_args=default_args, schedule_interval='@daily',
                                    ' ~/var/lib/mysql-files/raw_store_transactions_%s.csv' % yesterday_date)
 
 
+    t8 = EmailOperator(task_id='send_email',
+        to='mohammad.salimi.b@gmail.com',
+        subject='Daily report generated',
+        html_content=""" <h1>Congratulations! Your store reports are ready.</h1> """,
+        files=['/usr/local/airflow/store_files_airflow/location_wise_profit_%s.csv' % yesterday_date,
+               '/usr/local/airflow/store_files_airflow/store_wise_profit_%s.csv' % yesterday_date])
 
-    t1 >> t2 >> t3 >> t4 >> t5 >> [t6,t7] >> t8 >> t9
+    t9 = BashOperator(task_id='rename_raw',
+                      bash_command='mv ~/store_files_airflow/raw_store_transactions.csv'
+                                   ' ~/store_files_airflow/raw_store_transactions_%s.csv' % yesterday_date)
+
+    t1 >> t2 >> t3 >> t4 >> t5 >> [t6, t7] >> t8 >> t9
