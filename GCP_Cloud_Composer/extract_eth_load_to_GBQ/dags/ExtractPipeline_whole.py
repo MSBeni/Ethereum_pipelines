32ecca19a30eac13188905698dd88a944ec0f38e
MSBeni <andrei.sokurov.bitco@gmail.com>
2022-02-16 14:13:21 -0800

    Added the cloud composer codes 11

---
+++ GCP_Cloud_Composer/extract_eth_load_to_GBQ/dags/ExtractPipeline_whole.py
@@ -0,0 +1,87 @@
+'''
+pip3 install apache-airflow[gcp]
+'''
+from airflow import models
+from datetime import datetime, timedelta
+# from airflow.operators.python import PythonOperator
+from airflow.operators import python_operator
+from google.cloud import bigquery
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+
+def load_data():
+    # Set table_id to the ID of the table to create.
+    table_id = "avian-force-340302.eth_transaction.transactions2"
+    """
+        hash_ STRING,
+        nonce BIGINT,
+        block_hash STRING,
+        block_number BIGINT,
+        transaction_index BIGINT,
+        from_address STRING,
+        to_address STRING,
+        value DECIMAL,
+        gas BIGINT,
+        gas_price BIGINT,
+        input STRING,
+        block_timestamp INT64,
+        max_fee_per_gas INT64,
+        max_priority_fee_per_gas INT64,
+        transaction_type INT64
+    """
+    job_config = bigquery.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField("hash_", "STRING"),
+            bigquery.SchemaField("nonce", "INTEGER"),
+            bigquery.SchemaField("block_hash", "STRING"),
+            bigquery.SchemaField("block_number", "INTEGER"),
+            bigquery.SchemaField("transaction_index", "INTEGER"),
+            bigquery.SchemaField("from_address", "STRING"),
+            bigquery.SchemaField("to_address", "STRING"),
+            bigquery.SchemaField("value", "STRING"),
+            bigquery.SchemaField("gas", "INTEGER"),
+            bigquery.SchemaField("gas_price", "INTEGER"),
+            bigquery.SchemaField("input", "STRING"),
+            bigquery.SchemaField("block_timestamp", "INTEGER"),
+            bigquery.SchemaField("max_fee_per_gas", "FLOAT64"),
+            bigquery.SchemaField("max_priority_fee_per_gas", "FLOAT64"),
+            bigquery.SchemaField("transaction_type", "INTEGER"),
+        ],
+        skip_leading_rows=1,
+        # The source format defaults to CSV, so the line below is optional.
+        source_format=bigquery.SourceFormat.CSV,
+    )
+    uri = "gs://us-west1-eth-transactions-2168f060-bucket/transactions.csv"
+
+    load_job = client.load_table_from_uri(
+        uri, table_id, job_config=job_config
+    )  # Make an API request.
+
+    load_job.result()  # Waits for the job to complete.
+
+    destination_table = client.get_table(table_id)  # Make an API request.
+    print("Loaded {} rows.".format(destination_table.num_rows))
+
+
+default_args = {
+    'owner': 'Airflow',
+    'start_date': datetime(2022, 2, 16),
+    'retries': 0,
+    'retry_delay': timedelta(seconds=50),
+    'dataflow_default_options': {
+        'project': 'avian-force-340302',
+        'region': 'us-west1',
+        'runner': 'DataflowRunner'
+    }
+}
+
+with models.DAG('transactions_dag',
+                default_args=default_args,
+                schedule_interval='@daily',
+                catchup=False) as dag:
+    t1 = python_operator.PythonOperator(
+        task_id='ethtask',
+        python_callable=load_data,
+        # options={'input': 'gs://daily_food_orders/food_daily.csv'}
+    )
---
+++ web3_app/lastBlock_extractTransactions.py
@@ -0,0 +1,22 @@
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
+import json
+import pprint
+
+credentials = json.loads(open("../../infuraApp.json", 'r').read())
+
+infura_url = credentials['auth']['ENDPOINTS']
+
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+
+test_address = '0x8aecDAC523Eb208805780271BEBF19727F2f666F'
+
+print("isConnected:", web3.isConnected())
+
+latestBlock = web3.eth.getBlock('latest')
+pp = pprint.PrettyPrinter(indent=4)
+# print("Latest Block: ", latestBlock)
+# pp.pprint(dict(latestBlock).keys())
+
+print(dict(latestBlock)['transactions'][0])
